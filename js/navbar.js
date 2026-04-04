2aa3380164f5e2115735b71295aa64abb1e081e0
pseigne <71709682+pseigne@users.noreply.github.com>
2026-04-03 21:42:27 -0400

    added resume download

--- js/navbar.js
+++ js/navbar.js
@@ -2,4 +2,14 @@ fetch('navbar.html')
     .then(response => response.text())
     .then(html => {
         document.getElementById('navbar-placeholder').innerHTML = html;
+
+        const resumeDownloadLink = document.getElementById('resume-download-link');
+        if (resumeDownloadLink) {
+            resumeDownloadLink.addEventListener('click', (event) => {
+                const shouldDownload = window.confirm('Do you want to download your resume?');
+                if (!shouldDownload) {
+                    event.preventDefault();
+                }
+            });
+        }
     });
